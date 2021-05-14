dc90dd6a1181548f5cfd62ce3c1b97ff82aeee88
ignacioblanc <iiblanc@falabella.cl>
2021-05-14 16:08:26 -0400

    control new events and send a form control and added new component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import { AddCategory } from './components/AddCategory';
+const GifExpertApp = ()  => {
+    return  (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory/>
+    </>
+    );
+}
+
+export default GifExpertApp;
